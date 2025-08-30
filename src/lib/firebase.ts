4d1376165de1b25b416037f2b2154868ea4f837a
Syeda Qurrat <qurratnafees02@gmail.com>
2025-08-30 13:10:01 +0000

    acha ba btaao k sign up page k me jo auth not foud ka error aa rha hai ,

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDN3iiVX4O0QTR6VG8SuGPxzWeol2xI634",
+  authDomain: "studynest-e393f.firebaseapp.com",
+  projectId: "studynest-e393f",
+  storageBucket: "studynest-e393f.appspot.com",
+  messagingSenderId: "467497517477",
+  appId: "1:467497517477:web:dd13178904f1c934a4284c",
+  measurementId: "G-J0R0PYJRZ6"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
